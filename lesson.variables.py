d6db2e786dd7ff7a22f2ce8ade0ec77653eb226a
mohamad-meraj41 <kashamd40@gmail.com>
2025-02-25 12:18:25 +0330

    Course 1: Declaring and Initializing Variables

---
+++ lesson.variables.py
@@ -0,0 +1,32 @@
+# Course 1: Declaring and Initializing Variables
+# Attention: Variable names in Python cannot start with a number; they must begin with an alphabet letter.
+# You can use camelCase or an underscore (_) between words if using multiple words.
+
+# String variable (values should be in quotation marks)
+name = "John"
+last_name = "Anderson"
+
+# Print variables to check their values
+print(name)
+print(last_name)
+
+# Number variable (integers do not need quotation marks)
+age = 34
+height = 180
+
+print(age)
+print(height)
+
+# Boolean variable (can be either True or False)
+is_man = True
+admin = False
+
+print(is_man)
+print(admin)
+
+# Float variable (for decimal numbers)
+weight = 74.5
+grade = 10.8
+
+print(weight)
+print(grade)
